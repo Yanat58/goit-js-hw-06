0591a1b7557e49000ffd794e58d06c738d194c1c
Tetiana Karlova <tanyalyt80@gmail.com>
2023-02-25 00:26:44 +0200

    fixed

--- js/task-09.js
+++ js/task-09.js
@@ -4,11 +4,11 @@ const textColor = document.querySelector('.color');
 btn.addEventListener('click', onChangeColorsClick);
 
 function onChangeColorsClick() {
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
   const color = getRandomHexColor();
 
   document.body.style.backgroundColor = color;
   textColor.textContent = color;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
